bcb215a1a31a25af35eeebade38f76f4620a0a3c
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-13 14:37:43 -0300

    Feat: Criar método para executar a calculadora: Menu da calculadora

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Escolha uma operação: ");
+            System.out.println("1 - Adicionar");
+        }
+    }
+}
