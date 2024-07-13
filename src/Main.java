582013ee1263b06810903ec06f1f4411a6962bac
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-13 14:40:13 -0300

    Feat: Criar método para sair do programa na escolha 5- Sair, encerrar programa

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,14 @@ public class Main {
         while (true) {
             System.out.println("Escolha uma operação: ");
             System.out.println("1 - Adicionar");
+            System.out.println("2 - Subtrair");
+            System.out.println("3 - Multiplicar");
+            System.out.println("4 - Dividir");
+            System.out.println("5 - Sair");
+
+            int escolha = scanner.nextInt();
+
+            if (escolha == 5) {}
         }
     }
 }
