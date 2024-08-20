638b14d90a7a4d559dbc70f70a2e5651d1cacc4d
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-20 20:50:33 -0300

    Fix: após clicar em um operador o número desaparece

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,7 @@
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
-        Scanner scanner = new Scanner(System.in);
+        new CalculadoraGUI();
 
-        while (true) {
-            System.out.println("Escolha uma operação: ");
-            System.out.println("1. Adicionar");
-            System.out.println("2. Subtrair");
-            System.out.println("3. Multiplicar");
-            System.out.println("4. Dividir");
-            System.out.println("5. Ver Histórico");
-            System.out.println("6. Sair");
-
-            int escolha = scanner.nextInt();
-
-            if (escolha == 6) {
-                System.out.println("Encerrando o programa.");
-                break;
-            }
-
-            if (escolha == 5) {
-                List<String> historico = calculadora.getHistorico();
-                for (String operacao : historico) {
-                    System.out.println(operacao);
-                }
-                continue;
-            }
-
-            System.out.println("Digite o primeiro número:");
-            double num1 = scanner.nextDouble();
-
-            System.out.println("Digite o segundo número:");
-            double num2 = scanner.nextDouble();
-
-            double resultado = 0;
-            boolean operacaoValida = true;
-
-            switch (escolha) {
-                case 1:
-                    resultado = calculadora.adicionar(num1, num2);
-                    break;
-                case 2:
-                    resultado = calculadora.subtrair(num1, num2);
-                    break;
-                case 3:
-                    resultado = calculadora.multiplicar(num1, num2);
-                    break;
-                case 4:
-                    try {
-                        resultado = calculadora.dividir(num1, num2);
-                    } catch (ArithmeticException e) {
-                        System.out.println("Erro: " + e.getMessage());
-                        operacaoValida = false;
-                    }
-                    break;
-                default:
-                    System.out.println("Operação inválida.");
-                    operacaoValida = false;
-            }
-
-            if (operacaoValida) {
-                System.out.println("Resultado: " + resultado);
-            }
-        }
-
-        scanner.close();
     }
 }
