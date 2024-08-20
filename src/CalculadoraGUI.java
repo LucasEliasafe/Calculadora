51f380308e1407cf7abcf94fe21b82b2986bb1b9
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-20 20:41:00 -0300

    Feat: Adicionar os números dos botões, e definir o layout da calculadora

--- src/CalculadoraGUI.java
+++ src/CalculadoraGUI.java
@@ -9,6 +9,30 @@ public class CalculadoraGUI extends JFrame implements ActionListener{
     private double num1, num2;
     private String operacao;
 
-    public
+    public CalculadoraGUI(){
+        calculadora = new Calculadora();
+
+        setTitle("Calculadora");
+        setSize(400,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        display = new JTextField();
+        display.setFont(new Font("Arial", Font.PLAIN, 48));
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(5, 4));
+
+        String[] labels = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+",
+        };
+
+        for (String label : )
+    }
 
 }
