1e538edeeb8f89fcf6867142f7c9e9feae00b666
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-08-20 20:22:52 -0300

    Feat: criar classe CalculadoraGui para adicionar parte gráfica da calculadora usando swing

---
+++ src/CalculadoraGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculadoraGUI extends JFrame implements ActionListener{
+    private JTextField display;
+    private Calculadora calculadora;
+    private double num1, num2;
+    private String operacao;
+
+    public
+
+}
